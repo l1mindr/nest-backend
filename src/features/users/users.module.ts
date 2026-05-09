886e7bc51402a295f7027611a08d79aaffda96f2
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-05-09 11:16:25 +0330

    feat: add admin users controller

--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminUsersController } from './admin.users.controller';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
+  controllers: [UsersController, AdminUsersController],
   providers: [UsersService],
   exports: [UsersService]
 })
