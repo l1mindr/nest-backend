657f6debda055a24b49068995f0d2901c7aac5e3
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-05-09 14:53:30 +0330

    test: updated to agent test

---
+++ test/helpers/apiClient-helper.ts
@@ -0,0 +1,35 @@
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import TestAgent from 'supertest/lib/agent';
+
+type HttpMethod = 'get' | 'post' | 'put' | 'delete';
+type RequestOptions = {
+  method: HttpMethod;
+  url: string;
+  headers?: Record<string, string>;
+  query?: Record<string, unknown>;
+  body?: string | object;
+};
+export class ApiClient {
+  private readonly agent: InstanceType<typeof TestAgent>;
+
+  constructor(private readonly app: INestApplication) {
+    this.agent = request.agent(app.getHttpServer());
+  }
+
+  request({ method, url, headers, query, body }: RequestOptions) {
+    let req = this.agent[method](url);
+
+    if (headers) {
+      Object.entries(headers).forEach(([key, value]) => {
+        req = req.set(key, value);
+      });
+    }
+
+    if (query) req = req.query(query);
+
+    if (body) req = req.send(body);
+
+    return req;
+  }
+}
