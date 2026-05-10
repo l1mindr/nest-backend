4ea56f8fcdbcc3a80127a123f94189ddb6586d3d
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-05-10 20:02:53 +0330

    test: add user factory

--- test/factories/user.factory.ts
+++ test/factories/user.factory.ts
@@ -1,8 +1,83 @@
-export function createUser(overrides = {}) {
-  return {
-    email: 'test@test.com',
-    username: 'testuser',
-    password: 'Password@123',
-    ...overrides
-  };
+import { User } from '@features/users/entities/user.entity';
+import { INestApplication } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { ApiClient } from '../helpers/apiClient-helper';
+import { createUser } from '../helpers/create-user.helper';
+import {
+  AuthenticatedUserContext,
+  CreateUserContext,
+  LoginIdentifier
+} from '../utils/types/factory.types';
+import { TestUser } from '../utils/types/user.types';
+import { UserRole } from '@features/users/enums/user-role.enum';
+
+export class UserFactory {
+  static async create(
+    app: INestApplication,
+    overrides = {}
+  ): Promise<CreateUserContext> {
+    const user = createUser(overrides);
+    const client = new ApiClient(app);
+
+    const register = await client.request({
+      method: 'post',
+      url: '/v1/auth/register',
+      body: user
+    });
+
+    return {
+      user,
+      client,
+      response: {
+        register
+      }
+    };
+  }
+
+  static async authenticated(
+    app: INestApplication,
+    options: {
+      loginBy?: LoginIdentifier;
+      overrides?: Partial<TestUser>;
+    }
+  ): Promise<AuthenticatedUserContext> {
+    const context = await this.create(app, options.overrides);
+    const identifier = options.loginBy ?? 'email';
+
+    const login = await context.client.request({
+      method: 'post',
+      url: '/v1/auth/login',
+      body: {
+        email:
+          identifier === 'email' ? context.user.email : context.user.username,
+        password: context.user.password
+      }
+    });
+
+    return {
+      ...context,
+      response: {
+        ...context.response,
+        login
+      }
+    };
+  }
+
+  static async admin(
+    app: INestApplication,
+    dataSource: DataSource,
+    overrides = {}
+  ): Promise<AuthenticatedUserContext> {
+    const context = await this.authenticated(app, overrides);
+    const repo = dataSource.getRepository(User);
+
+    await repo.update(
+      { email: context.user.email },
+      {
+        role: UserRole.ADMIN
+      }
+    );
+
+    return context;
+  }
 }
