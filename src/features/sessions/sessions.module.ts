6ac170165dfbd963116caf41256d278e5972038b
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-05-12 19:50:59 +0330

    refactor: add hashing provider

--- src/features/sessions/sessions.module.ts
+++ src/features/sessions/sessions.module.ts
@@ -5,13 +5,21 @@ import { SessionsController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from '@features/auth/config/jwt.config';
+import { HashingProvider } from '@features/auth/providers/hashing.provider';
+import { BcryptProvider } from '@features/auth/providers/bcrypt.provider';
 
 @Module({
   imports: [
     JwtModule.registerAsync(jwtConfig.asProvider()),
     TypeOrmModule.forFeature([Session])
   ],
-  providers: [SessionsService],
+  providers: [
+    SessionsService,
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider
+    }
+  ],
   controllers: [SessionsController],
   exports: [SessionsService]
 })
