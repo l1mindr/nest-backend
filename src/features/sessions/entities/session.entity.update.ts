51ce4706e2b4537948a0909d3e141ea39bca6464
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-05-12 13:37:49 +0330

    test: update file

---
+++ src/features/sessions/entities/session.entity.update.ts
@@ -0,0 +1,54 @@
+import { User } from '@features/users/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm';
+import { IUserAgent } from '../interfaces/user-agent.interface';
+import { SwaggerSessionPropertiesUpdate as SessionProps } from '../sessions.swagger';
+
+@Entity()
+export class Session {
+  @ApiProperty(SessionProps.id)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty(SessionProps.refreshTokenHash)
+  @Column({ unique: true })
+  refreshTokenHash: string;
+
+  @ApiProperty(SessionProps.userAgent)
+  @Column({ type: 'json' })
+  userAgent: IUserAgent;
+
+  @ApiProperty(SessionProps.ipAddress)
+  @Column()
+  ipAddress: string;
+
+  @Column({ default: false })
+  isRevoked: boolean;
+
+  @ApiProperty(SessionProps.expireAt)
+  @Column()
+  expireAt: Date;
+
+  @ApiProperty(SessionProps.lastUsedAt)
+  @Column()
+  lastUsedAt: Date;
+
+  @ApiProperty(SessionProps.createdAt)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty(SessionProps.updatedAt)
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ApiProperty(SessionProps.user)
+  @ManyToOne(() => User, (user) => user.sessions, { nullable: false })
+  owner: User;
+}
