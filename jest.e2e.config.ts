ff187f91166daffc50686d402d46124961e1172a
Mohamadreza Irick <l1mindr@proton.me>
2026-05-18 00:07:51 +0330

    fix(config): add jest for unit and e2e tests

---
+++ jest.e2e.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  }),
+  rootDir: '.',
+  testMatch: ['**/*.e2e-spec.ts'],
+  testTimeout: 30000
+};
+
+export default config;
