02a97b62fcc61bf5f57047c15711d690698ec5ad
Mohamadreza Irick <l1mindr@proton.me>
2026-05-23 23:26:23 +0330

    refactor(app): import SharedModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { CoreModule } from '@core/core.module';
 import { FeaturesModule } from '@features/features.module';
 import { InfrastructureModule } from '@infrastructure/infrastructure.module';
 import { Module } from '@nestjs/common';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [CoreModule, InfrastructureModule, FeaturesModule]
+  imports: [SharedModule, CoreModule, InfrastructureModule, FeaturesModule]
 })
 export class AppModule {}
