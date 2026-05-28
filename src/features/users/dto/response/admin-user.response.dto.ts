3cd07447cc3e75e98ee85955cd5317fa2e7c0a1a
Mohamadreza Irick <l1mindr@proton.me>
2026-05-28 20:10:06 +0330

    feat(dto): enhance AdminUserResponseDto and UserProfileResponseDto with Transform for registryDates

--- src/features/users/dto/response/admin-user.response.dto.ts
+++ src/features/users/dto/response/admin-user.response.dto.ts
@@ -2,13 +2,13 @@ import { TimestampResponseDto } from '@core/common/dto/timestamp-response.dto';
 import { UserRole } from '@features/users/enums/user-role.enum';
 import { UserStatus } from '@features/users/enums/user-status.enum';
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 
 export class AdminUserResponseDto extends TimestampResponseDto {
-  @Expose()
   @ApiProperty({
     example: 'uuid'
   })
+  @Expose()
   id: string;
 
   @ApiProperty({
@@ -17,33 +17,35 @@ export class AdminUserResponseDto extends TimestampResponseDto {
     nullable: true,
     required: false
   })
+  @Expose()
   name: string | null;
 
-  @Expose()
   @ApiProperty({
     example: 'john_doe'
   })
+  @Expose()
   username: string;
 
-  @Expose()
   @ApiProperty({
     example: 'john@example.com'
   })
+  @Expose()
   email: string;
 
-  @Expose()
   @ApiProperty({
     enum: UserRole
   })
+  @Expose()
   role: UserRole;
 
-  @Expose()
   @ApiProperty({
     enum: UserStatus
   })
+  @Expose()
   status: UserStatus;
 
-  @Expose({ name: 'createdAt' })
+  @Expose()
+  @Transform(({ obj }) => obj.registryDates.createdAt)
   @ApiProperty({
     example: '2024-10-01T12:34:56.000Z'
   })
--- src/features/users/dto/response/user-profile.response.dto.ts
+++ src/features/users/dto/response/user-profile.response.dto.ts
@@ -1,44 +1,45 @@
 import { TimestampResponseDto } from '@core/common/dto/timestamp-response.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { UserRole } from '../../enums/user-role.enum';
 
 export class UserProfileResponseDto extends TimestampResponseDto {
-  @Expose()
   @ApiProperty({
     example: 'John Doe',
     description: 'Full name of the user',
     nullable: true,
     required: false
   })
+  @Expose()
   name: string | null;
 
-  @Expose()
   @ApiProperty({
     example: 'john_doe',
     description: 'Unique username'
   })
+  @Expose()
   username: string;
 
-  @Expose()
   @ApiProperty({
     example: 'john@example.com',
     description: 'User email address'
   })
+  @Expose()
   email: string;
 
-  @Expose()
   @ApiProperty({
     enum: UserRole,
     example: UserRole.USER,
     description: 'User role'
   })
+  @Expose()
   role: UserRole;
 
-  @Expose({ name: 'createdAt' })
   @ApiProperty({
     example: '2024-10-01T12:34:56.000Z',
     description: 'Registration date'
   })
+  @Expose()
+  @Transform(({ obj }) => obj.registryDates.createdAt)
   joinedAt: Date;
 }
