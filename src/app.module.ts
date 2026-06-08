0d64bf7b62ae380cba2fc9a66d1443e67cb19230
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-06-08 17:43:10 +0330

    feat(device): register device module and middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { CoreModule } from '@core/core.module';
+import { DeviceMiddleware } from '@core/device/device.middleware';
 import { FeaturesModule } from '@features/features.module';
 import { InfrastructureModule } from '@infrastructure/infrastructure.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 
 @Module({
   imports: [CoreModule, InfrastructureModule, FeaturesModule]
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DeviceMiddleware).forRoutes('*');
+  }
+}
