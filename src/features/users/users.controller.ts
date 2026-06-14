02fdbda12307854bdad417f1dd31480e115ca187
l1mindr <mohamadresaaa@gmail.com>
2026-06-14 21:32:05 +0330

    refactor(users): update users controller

--- src/features/users/users.controller.ts
+++ src/features/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { User } from '@features/auth/decorators/user.decorator';
+import { Serialize } from '@core/common/decorators/serialize.decorator';
+import { User } from '@features/security/decorators/user.decorator';
 import { User as UserEntity } from '@features/users/entities/user.entity';
 import {
   Body,
@@ -10,15 +11,14 @@ import {
   Put
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateUserRequestDto } from './dto/request/update-user.request.dto';
+import { UserProfileResponseDto } from './dto/response/user-profile.response.dto';
 import { UsersService } from './users.service';
 import {
   ApiChangeProfile,
   ApiDeleteAccount,
   ApiGetProfile
 } from './users.swagger';
-import { Serialize } from '@core/common/decorators/serialize.decorator';
-import { UserProfileResponseDto } from './dto/response/user-profile.response.dto';
-import { UpdateUserRequestDto } from './dto/request/update-user.request.dto';
 
 @Controller({
   path: 'user',
@@ -32,24 +32,21 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   @ApiGetProfile()
   @Serialize(UserProfileResponseDto)
-  getProfile(@User('user') user: UserEntity) {
+  getProfile(@User() user: UserEntity) {
     return user;
   }
 
   @Put()
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiChangeProfile()
-  changeProfile(
-    @User('user') user: UserEntity,
-    @Body() dto: UpdateUserRequestDto
-  ) {
+  changeProfile(@User() user: UserEntity, @Body() dto: UpdateUserRequestDto) {
     return this.usersService.updateProfile(user.id, dto);
   }
 
   @Delete('delete-account')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiDeleteAccount()
-  deleteAccount(@User('user') user: UserEntity) {
+  deleteAccount(@User() user: UserEntity) {
     return this.usersService.requestAccountDeletion(user.id);
   }
 }
