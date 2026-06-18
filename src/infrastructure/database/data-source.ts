84c4ea11e9d1aed2e0f12eb5a32a9ae74001c754
l1mindr <mohamadresaaa@gmail.com>
2026-06-18 12:31:56 +0330

    refactor(database): simplify data source env configuration handling

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -1,12 +1,15 @@
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 import { DataSource } from 'typeorm';
+import databaseConfig from './config/database.config';
 
 dotenvExpand.expand(dotenv.config());
 
+const config = databaseConfig();
+
 export default new DataSource({
   type: 'postgres',
-  url: process.env.DATA_SOURCE_URL,
+  url: config.url,
   entities: ['dist/features/**/*.entity{.ts,.js}'],
   migrations: ['dist/infrastructure/database/migrations/*{.ts,.js}']
 });
