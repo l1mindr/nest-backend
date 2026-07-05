22aaec657a460dc765609601027df9aaff180608
l1mindr <mohamadresaaa@gmail.com>
2026-07-05 15:40:25 +0330

    feat(decorator): add ToBoolean decorator and utility function for boolean transformation

--- src/infrastructure/http/dto/remove.dto.ts
+++ src/infrastructure/http/dto/remove.dto.ts
@@ -1,6 +1,6 @@
+import { ToBoolean } from '@infrastructure/http/validation/decorators/to-boolean.decorator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
-import { ToBoolean } from '@core/transforms/decorators/to-boolean.decorator';
 
 export class RemoveDto {
   @ApiPropertyOptional({
--- src/infrastructure/http/validation/decorators/to-boolean.decorator.ts
+++ src/infrastructure/http/validation/decorators/to-boolean.decorator.ts
@@ -1,4 +1,4 @@
 import { Transform } from 'class-transformer';
-import { toBoolean } from '../utils/to-boolean';
+import { toBoolean } from '../../../../core/utils/to-boolean';
 
 export const ToBoolean = () => Transform(({ value }) => toBoolean(value));
