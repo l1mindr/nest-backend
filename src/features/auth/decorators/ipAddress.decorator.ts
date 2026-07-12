e8a95f300441a44aab0e7207b950fc5d79e3598d
l1mindr <mohamadresaaa@gmail.com>
2026-07-12 15:27:39 +0330

    refactor(decorator): simplify IpAddress decorator to return request.ip directly

--- src/features/auth/decorators/ipAddress.decorator.ts
+++ src/features/auth/decorators/ipAddress.decorator.ts
@@ -5,12 +5,6 @@ export const IpAddress = createParamDecorator(
   (_: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<IRequest>();
 
-    const ip = request.headers['x-forwarded-for'] as string;
-
-    if (ip && process.env.NODE_ENV === 'production') {
-      return ip.split(',')[0].replace(',', '');
-    }
-
-    return ip ?? '127.0.0.1';
+    return request.ip;
   }
 );
