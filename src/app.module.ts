6f8b67aaa45a2baf0091d285d172c3f177eff4cd
l1mindr <mohamadresaaa@gmail.com>
2026-07-13 18:53:34 +0330

    feat(logging): integrate LoggingModule and enhance application startup logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { CoreModule } from '@core/core.module';
 import { FeaturesModule } from '@features/features.module';
 import { InfrastructureModule } from '@infrastructure/infrastructure.module';
+import { LoggingModule } from '@infrastructure/logging/logging.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [CoreModule, InfrastructureModule, FeaturesModule]
+  imports: [LoggingModule, CoreModule, InfrastructureModule, FeaturesModule]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { setupApp } from './bootstrap';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { LogEvent } from './infrastructure/logging/logging.constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true
+  });
+
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+
   setupApp(app);
   await app.listen(8080);
+
+  logger.log(
+    {
+      event: LogEvent.APPLICATION_STARTUP,
+      port: 8080,
+      environment: process.env.NODE_ENV
+    },
+    'Application started'
+  );
 }
 
 bootstrap();
