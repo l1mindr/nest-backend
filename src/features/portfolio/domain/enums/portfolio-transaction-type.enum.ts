20120cd99af41190ad9caa5764257f008983c034
l1mindr <mohamadresaaa@gmail.com>
2026-08-09 13:40:05 +0330

    feat(portfolio): add portfolio transaction domain

---
+++ src/features/portfolio/domain/enums/portfolio-transaction-type.enum.ts
@@ -0,0 +1,8 @@
+export enum PortfolioTransactionType {
+  BUY = 'BUY',
+  SELL = 'SELL',
+  TRANSFER_IN = 'TRANSFER_IN',
+  TRANSFER_OUT = 'TRANSFER_OUT',
+  DEPOSIT = 'DEPOSIT',
+  WITHDRAWAL = 'WITHDRAWAL'
+}
